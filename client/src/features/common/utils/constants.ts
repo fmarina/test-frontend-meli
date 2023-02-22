fb317efe057d33f0658e22f16abe60d9ae15efad
marinafaenze <marinafaenze@hotmail.com>
2023-02-22 02:39:38 -0300

    create constants file

---
+++ client/src/features/common/utils/constants.ts
@@ -0,0 +1,8 @@
+export const ROOT_PATH = '/'
+export const PRODUCT_LIST_PATH = '/items'
+export const PRODUCT_DETAIL_PATH = '/items/:id'
+
+export const API_URL = process.env.REACT_APP_API_URL
+export const PRODUCT_API_URL = `${API_URL}/api/items`
+
+export const NEW_CONDITION = 'new'
